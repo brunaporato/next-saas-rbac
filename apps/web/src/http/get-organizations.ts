68e2860da7643e45fbebdb6dca4e3662f6947562
brunaporato <brunaporato@gmail.com>
2024-09-27 08:19:01 -0300

    feat: project switcher component:

--- apps/web/src/http/get-organizations.ts
+++ apps/web/src/http/get-organizations.ts
@@ -1,14 +1,12 @@
 import { api } from './api-client'
 
 interface GetOrganizationsResponse {
-  organizations: [
-    {
-      id: string
-      name: string
-      slug: string
-      avatarUrl: string | null
-    },
-  ]
+  organizations: {
+    id: string
+    name: string
+    slug: string
+    avatarUrl: string | null
+  }[]
 }
 
 export async function getOrganizations() {
