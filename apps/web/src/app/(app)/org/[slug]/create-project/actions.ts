067d58aa7f0bfb52bbf518b142f93326e0566b17
brunaporato <brunaporato@gmail.com>
2024-10-02 07:17:43 -0300

    styles: create project page

---
+++ apps/web/src/app/(app)/org/[slug]/create-project/actions.ts
@@ -0,0 +1,50 @@
+'use server'
+
+import { HTTPError } from 'ky'
+import { z } from 'zod'
+
+// import { createProject } from '@/http/create-project'
+
+const projectSchema = z.object({
+  name: z.string().min(4, { message: 'Please include at least 4 characters' }),
+  description: z.string(),
+})
+
+export async function createProjectAction(data: FormData) {
+  const result = projectSchema.safeParse(Object.fromEntries(data))
+
+  if (!result.success) {
+    const errors = result.error.flatten().fieldErrors
+    return { success: false, message: null, errors }
+  }
+
+  const { name, description } = result.data
+
+  try {
+    // await createProject({
+    //   name,
+    //   domain,
+    //   shouldAttachUsersByDomain,
+    // })
+  } catch (error) {
+    if (error instanceof HTTPError) {
+      const { message } = await error.response.json()
+
+      return { success: false, message, errors: null }
+    }
+
+    console.log(error)
+
+    return {
+      success: false,
+      message: 'An unexpected error occurred. Try again later.',
+      errors: null,
+    }
+  }
+
+  return {
+    success: true,
+    message: 'Successfully saved the project',
+    errors: null,
+  }
+}
