b2d9e3f7977450036a8f769ee4ef4683744cfb0b
brunaporato <brunaporato@gmail.com>
2024-10-08 08:23:54 -0300

    feat: accepting invites

--- apps/web/src/app/invite/[id]/page.tsx
+++ apps/web/src/app/invite/[id]/page.tsx
@@ -1,9 +1,16 @@
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
+import { CheckCircle, LogIn, LogOut } from 'lucide-react'
+import { cookies } from 'next/headers'
+import Link from 'next/link'
+import { redirect } from 'next/navigation'
 
+import { auth, isAuthenticated } from '@/auth/auth'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import { Button } from '@/components/ui/button'
+import { Separator } from '@/components/ui/separator'
+import { acceptInvite } from '@/http/accept-invite'
 import { getInvite } from '@/http/get-invite'
-import { Separator } from "@/components/ui/separator"
 
 interface InvitePageProps {
   params: {
@@ -19,6 +26,33 @@ export default async function InvitePage({ params }: InvitePageProps) {
   const inviteId = params.id
 
   const { invite } = await getInvite(inviteId)
+  const isUserAuthenticated = isAuthenticated()
+
+  let currentUserEmail = null
+
+  if (isUserAuthenticated) {
+    const { user } = await auth()
+
+    currentUserEmail = user.email
+  }
+
+  const userIsAuthenticatedWithSameEmailFromInvite =
+    currentUserEmail === invite.email
+
+  async function signInFromInvite() {
+    'use server'
+
+    cookies().set('inviteId', inviteId)
+
+    redirect(`/auth/sign-in?email=${invite.email}`)
+  }
+
+  async function acceptInviteAction() {
+    'use server'
+
+    await acceptInvite(inviteId)
+    redirect('/')
+  }
 
   return (
     <div className="flex min-h-screen flex-col items-center justify-center px-4">
@@ -47,7 +81,52 @@ export default async function InvitePage({ params }: InvitePageProps) {
 
         <Separator />
 
-        
+        {!isUserAuthenticated && (
+          <form action={signInFromInvite}>
+            <Button type="submit" variant="secondary" className="w-full">
+              <LogIn className="mr-2 size-4" />
+              Sign in to accept the invite
+            </Button>
+          </form>
+        )}
+
+        {userIsAuthenticatedWithSameEmailFromInvite && (
+          <form action={acceptInviteAction}>
+            <Button type="submit" variant="secondary" className="w-full">
+              <CheckCircle className="mr-2 size-4" />
+              Join {invite.organization.name}
+            </Button>
+          </form>
+        )}
+
+        {isUserAuthenticated && !userIsAuthenticatedWithSameEmailFromInvite && (
+          <div className="space-y-4">
+            <p className="text-balance text-center text-sm leading-relaxed text-muted-foreground">
+              This invite was sent to{' '}
+              <span className="font-medium text-foreground">
+                {invite.email}
+              </span>{' '}
+              but you are currently authenticated as{' '}
+              <span className="font-medium text-foreground">
+                {currentUserEmail}
+              </span>
+              .
+            </p>
+
+            <div className="space-y-2">
+              <Button className="w-full" variant="secondary" asChild>
+                <a href="/api/auth/sign-out">
+                  <LogOut className="mr-2 size-4" />
+                  Sign out from {currentUserEmail}
+                </a>
+              </Button>
+
+              <Button className="w-full" variant="outline" asChild>
+                <Link href="/">Back to dashboard</Link>
+              </Button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
